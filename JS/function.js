9615f1fef97f645c6767d3ed661a29e5bfea775f
val619 <90335198+StyleDarkLight@users.noreply.github.com>
2022-04-20 10:52:54 +0200

    lezgongue

---
+++ JS/function.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("nav").style.width = "20vw";
+    document.body.style.transform = 'translate(20vw)';
+}
+function closeNav() {
+    document.getElementById("nav").style.width = '0';
+    document.body.style.transform = 'translate(0vw)';
+}
